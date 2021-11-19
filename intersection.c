8d35ad8472d91246934b4e3fc9c3dc88257f47a6
codejune <kbj9704@gmail.com>
2021-11-20 00:51:02 +0900

    Bug detect: Queue put method doesn't working

--- intersection.c
+++ intersection.c
@@ -11,5 +11,264 @@
  */
 int main(void)
 {
+    pthread_t way_tid[MAX_WAY_COUNT][2];
+    pthread_t intrsect_tid;
+    uint8_t i, ret;
+
+    // Initialize intersection
+    init_intrsect();
+
+    // Initialize vehicle list
+    init_vhcle_q();
+
+    // Initialize way queue
+    init_way_q();
+
+    for (i = 0; i < g_vhcle_cnt; i++)
+    {
+        printf("%hhd\n", g_vhcle_q.data[i]);
+    }
+    // Generate way thread
+    for (i = 0; i < MAX_WAY_COUNT; i++)
+    {
+        way_tid[i][1] = i + 1;
+        if ((pthread_create(&(way_tid[i][0]), NULL, &t_way, (void *)&(way_tid[i][1]))) != 0)
+        {
+            fprintf(stderr, "pthread_create() error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Generate intersection thread
+    if ((ret = pthread_create(&intrsect_tid, NULL, &t_intrsect, NULL)) != 0)
+    {
+        fprintf(stderr, "pthread_create() error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Wait for traffic is done
+    if ((ret = pthread_join(intrsect_tid, NULL)) != 0)
+    {
+        fprintf(stderr, "pthread_join() error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Destroy all way thread
+    for (i = 0; i < MAX_WAY_COUNT; i++)
+        if ((ret = pthread_cancel(way_tid[i][0])) != 0)
+        {
+            fprintf(stderr, "pthread_cancel() error\n");
+            exit(EXIT_FAILURE);
+        }
+
+    // De-allocation all way queue
+    free(g_way_q);
+
     exit(EXIT_SUCCESS);
+}
+
+/**
+ * @brief Initialize intersection
+ */
+void init_intrsect(void)
+{
+    g_intrsect.traffic_type = TRAFFIC_TYPE_NOT_RUNNING;
+    memset(g_intrsect.is_running, false, sizeof(g_intrsect.is_running));
+}
+
+/**
+ * @brief Initialize vehicle number and list
+ */
+void init_vhcle_q(void)
+{
+    uint8_t i;
+#ifdef DEBUG
+    uint8_t test_data[10] = {4, 4, 3, 4, 1, 2, 2, 3, 3, 3};
+    g_vhcle_cnt = 10;
+#else
+    uint8_t way;
+
+    // Get vehicle count
+    printf("Total number of vehicles: ");
+    scanf("%hhd", &g_vhcle_cnt);
+
+    if (g_vhcle_cnt > MAX_VHCLE_COUNT)
+    {
+        fprintf(stderr, "Out of maximum vehicle count\n");
+        exit(EXIT_FAILURE);
+    }
+
+    srand(time(NULL));
+#endif
+
+    // Generate vehicle list and print
+    printf("Start point: ");
+    for (i = 0; i < g_vhcle_cnt; i++)
+    {
+#ifndef DEBUG
+        way = rand() % MAX_VHCLE_COUNT + 1;
+        printf("%d ", way);
+        q_put(g_vhcle_q, way);
+#else
+        printf("%d ", test_data[i]);
+        q_put(g_vhcle_q, test_data[i]);
+#endif
+    }
+    printf("\n");
+}
+
+/**
+ * @brief Initialize way queue
+ */
+void init_way_q(void)
+{
+    uint8_t i;
+
+    g_way_q = (queue_t *)malloc(sizeof(queue_t) * g_vhcle_cnt);
+
+    for (i = 0; i < g_vhcle_cnt; i++)
+    {
+        g_way_q[i].items = 0;
+        g_way_q[i].head = 0;
+        g_way_q[i].tail = 0;
+    }
+}
+
+/**
+ * @brief Get boolean of queue is empty
+ * @param q Target queue
+ * @return true Queue is empty, false Queue is not empty
+ */
+bool q_is_empty(queue_t q)
+{
+    return q.items == 0 ? true : false;
+}
+
+/**
+ * @brief Get boolean of queue is full
+ * @param q Target queue
+ * @return true Queue is full, false Queue is not full
+ */
+bool q_is_full(queue_t q)
+{
+    return q.items == MAX_QUEUE_SIZE ? true : false;
+}
+
+/**
+ * @brief Put data into queue
+ * @param q Target queue
+ * @param data Way number
+ */
+void q_put(queue_t q, uint8_t data)
+{
+    q.data[q.head] = data;
+    q.head = (q.head + 1) % MAX_QUEUE_SIZE;
+    q.items++;
+}
+
+/**
+ * @brief Get data from queue
+ * @param q Target queue
+ * @return uint8_t Way number
+ */
+uint8_t q_get(queue_t q)
+{
+    uint8_t data;
+    data = q.data[q.tail];
+    q.tail = (q.tail + 1) % MAX_QUEUE_SIZE;
+    q.items--;
+    return data;
+}
+
+void *t_intrsect(void *arg)
+{
+    uint8_t tick, way;
+
+    tick = 1;
+
+    // Traffic signal routine
+    while (true)
+    {
+        printf("tick: %hhd\n", tick);
+
+        // Get vehicle from queue and put it on the way
+        if (!q_is_empty(g_vhcle_q))
+        {
+            way = q_get(g_vhcle_q);
+            q_put(g_way_q[way - 1], way);
+        }
+
+        // Send signal to way which is send vehicle to the intersection
+        pthread_cond_broadcast(&g_tf_cond);
+
+        // pthread_mutex_lock(&g_mutex);
+
+        // Wait for signal from way which is receive vehicle from the intersection
+        pthread_cond_wait(&g_w_cond, &g_mutex);
+
+        printf("==========================\n");
+        printf("Passed Vehicle\n");
+        printf("Car ");
+        printf("Waiting Vehicle\n");
+        printf("Car ");
+        printf("==========================\n");
+
+        pthread_mutex_unlock(&g_mutex);
+    }
+}
+
+/**
+ * @brief Way thread function, which is send vehicle to the intersection
+ * @param arg Way number
+ * @return void* NULL
+ */
+void *t_way(void *arg)
+{
+    uint8_t way;
+
+    way = *((int *)arg);
+
+    while (true)
+    {
+        pthread_cond_wait(&g_tf_cond, &g_mutex);
+
+        if (q_is_empty(g_way_q[way]))
+        {
+            pthread_mutex_unlock(&g_mutex);
+            continue;
+        }
+
+        // Set vehicle to passing status
+        // Traffic type is not running
+        if (g_intrsect.traffic_type == TRAFFIC_TYPE_NOT_RUNNING)
+        {
+            g_intrsect.traffic_type = way % 2;
+            g_intrsect.passing[0][0] = q_get(g_way_q[way]);
+            g_intrsect.passing[0][1] = 2;
+            g_intrsect.is_running[0] = true;
+        }
+        // Traffic type is matched
+        else if (g_intrsect.traffic_type == way % 2)
+        {
+            // Road is not full and there is no same start way
+            if (g_intrsect.is_running[0] && g_intrsect.passing[0][0] != way && !g_intrsect.is_running[1])
+            {
+                g_intrsect.passing[1][0] = q_get(g_way_q[way]);
+                g_intrsect.passing[1][1] = 2;
+                g_intrsect.is_running[1] = true;
+            }
+            else if (g_intrsect.is_running[1] && g_intrsect.passing[1][0] != way && !g_intrsect.is_running[0])
+            {
+                g_intrsect.passing[0][0] = q_get(g_way_q[way]);
+                g_intrsect.passing[0][1] = 2;
+                g_intrsect.is_running[0] = true;
+            }
+        }
+
+        // Raise signal to intersection that the vehicle is passing
+        pthread_cond_signal(&g_w_cond);
+
+        // Unlock pthread mutex
+        pthread_mutex_unlock(&g_mutex);
+    }
 }
--- intersection.h
+++ intersection.h
@@ -12,18 +12,66 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <errno.h>
-#include <dirent.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/syscall.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
 
 // Macro
+#define MAX_VHCLE_COUNT 15                 // Maximal vehicle count
+#define MAX_WAY_COUNT 4                    // Maximal way count
+#define MAX_QUEUE_SIZE MAX_VHCLE_COUNT - 1 // Maximal queue size
+#define DEBUG 1                            // Debug mode
+// Structure
+/**
+ * @brief Traffic type enumeration
+ */
+typedef enum traffic_t
+{
+    TRAFFIC_TYPE_HORIZONTAL = 0, // P1, P3
+    TRAFFIC_TYPE_VERTICAL,       // P2, P4
+    TRAFFIC_TYPE_NOT_RUNNING,    // Not running
+} traffic_t;
 
+/**
+ * @brief Cycle queue structure
+ */
+typedef struct queue_t
+{
+    uint8_t data[MAX_VHCLE_COUNT]; // Queue data
+    uint8_t items;                 // Number of items
+    uint8_t head;                  // Head index
+    uint8_t tail;                  // Tail index
+} queue_t;
+
+/**
+ * @brief Intersection structure
+ */
+typedef struct intrsect_t
+{
+    uint8_t passing[2][2];       // Passing road, left time of passing
+    enum traffic_t traffic_type; // Traffic type
+    bool is_running[2];          // Way status
+} intrsect_t;
 
 // Global Variable
+pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER; // Mutex lock variable
+pthread_cond_t g_tf_cond = PTHREAD_COND_INITIALIZER; // Traffic Signal condition variable
+pthread_cond_t g_w_cond = PTHREAD_COND_INITIALIZER;  // Way condition variable
+uint8_t g_vhcle_cnt;                                 // Vehicle count
+queue_t g_vhcle_q;                                   // Vehicle queue
+queue_t *g_way_q;                                    // Way queue list
+intrsect_t g_intrsect;                               // Intersection structure
+uint8_t g_passed_vhcle[MAX_WAY_COUNT] = {0};         // Passed vehicle
 
 // Function prototype
-
-
+void init_intrsect(void);
+void init_vhcle_q(void);
+void init_way_q(void);
+bool q_is_empty(queue_t q);
+bool q_is_full(queue_t q);
+void q_put(queue_t q, uint8_t data);
+uint8_t q_get(queue_t q);
+void *t_intrsect(void *arg);
+void *t_way(void *arg);
 #endif
