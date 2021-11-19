4817aa8e0a11323f6e97d5dc5796a4de28527a66
codejune <kbj9704@gmail.com>
2021-11-19 16:06:09 +0900

    init

---
+++ intersection.c
@@ -0,0 +1,15 @@
+/**
+ * @file intersection.c
+ * @brief 프로젝트에서 사용되는 함수 정의
+ * @author 김병준 (kbj9704@gmail.com)
+ */
+#include "intersection.h"
+
+/**
+ * @brief Main entry function
+ * @return int OK 0, ERROR 1
+ */
+int main(void)
+{
+    exit(EXIT_SUCCESS);
+}
---
+++ intersection.h
@@ -0,0 +1,29 @@
+/**
+ * @file intersection.h
+ * @author 김병준 (kbj9704@gmail.com)
+ * @brief 프로그램 구현에 필요한 공통 헤더, 함수 및 매크로 정의
+ */
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+// header
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <errno.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+
+// Macro
+
+
+// Global Variable
+
+// Function prototype
+
+
+#endif
